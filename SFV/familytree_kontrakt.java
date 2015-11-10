ac8a1a2fdb84bef511f20c289082ca5fff00390d
Ibsylong <luanbanguyen94@gmail.com>
2015-11-10 13:51:11 +0100

    Create familytree_kontrakt.java

---
+++ SFV/familytree_kontrakt.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+
+public interface familytree {
+    public List<String> Sort();
+
+    public void Read_file(String filename);
+}
