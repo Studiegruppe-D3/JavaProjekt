69d3b12561d9fbc723118dc26602803208875ae1
Staerdahl <peterandersen14@live.dk>
2015-11-10 13:19:45 +0100

    Update backend.java

--- SFV/src/backend.java
+++ SFV/src/backend.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.Writer;
 
-public class backend {
+public class Sort implements FamilyTree {
 	
 	public static ArrayList read_file(){
 		ArrayList list = new ArrayList();
