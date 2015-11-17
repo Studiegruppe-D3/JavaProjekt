73121c1eb736cbf173336c32f78f8a8e95f61421
Timmy <zlyzart@gmail.com>
2015-11-17 10:45:17 +0100

    Update frontend.java

--- frontend.java
+++ frontend.java
@@ -2,27 +2,21 @@ import java.util.*;
 import java.io.*;
 
 public class frontend {
-
-public static void main(String[] args) {
-	
-		File file = new File("Names.txt");
-
-	    try 
-	    		{
-
-	        Scanner sc = new Scanner(file);
-
-	        while (sc.hasNextLine()) {
-	            String i = sc.nextLine();
-	            
-	            String[] str = i.split(" ");
-	            
-	            
-	            System.out.println(str[0]+" "+ str[1]);
-	            }
-	        sc.close();
-	    } 
-	    catch (FileNotFoundException e) 
-	    { e.printStackTrace(); }
-	}
+    public static void main(String[] args) {
+        File file = new File("src/sorted.txt");
+        try
+        {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                String[] str = i.split(" ");
+                String strr = str[0];
+                strr = strr.substring(0, 1);
+                System.out.println(strr+" "+ str[1]);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e)
+        { e.printStackTrace(); }
+    }
 }
